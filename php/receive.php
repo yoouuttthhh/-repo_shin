e6e596d88fb2eb236064a34244d6d8f1140e6bc0
yoouuttthhh <hessen.oct@gmail.com>
2025-03-23 16:03:37 +0900

    javascript.jsの作成と作業の途中

--- php/receive.php
+++ php/receive.php
@@ -1,3 +1,30 @@
 <?php
     print_r($_POST);
 ?>
+
+
+<?php
+//$a = 'いちばんやさしい';
+//$b = 'php';
+//$title = $a . $b;
+//echo $title;
+?>  
+
+<?php
+//$a = 'サンプル';
+//echo "テストです$a<br>";
+//echo 'テスト$a';
+?>
+
+<?php
+//$fruits = ['りんご','バナナ','メロン'];
+//echo $fruits[0];
+//echo $fruits[1];
+?>
+
+<?php
+//$home = ['adress1'=>'東京都','adress2'=>'千代田区','tel'=>'03-1234-5678'];
+//echo $home['adress1'];
+//echo $home['adress2'];
+//echo $home['tel'];
+?>
